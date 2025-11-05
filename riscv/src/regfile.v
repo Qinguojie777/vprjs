a8344056f4ca5ec9c3dd619e1b0dc8723787501c
hqqgj <qinguojie862@qq.com>
2025-11-05 17:10:16 +0800

    iniial regfile.v

--- riscv/src/regfile.v
+++ riscv/src/regfile.v
@@ -13,9 +13,10 @@ module regfile(
     input   [`RegAddrBus-1:0]   raddr2,
     output  [`RegDataBus-1:0]   rdata2
 );
-reg [`RegDataBus-1:0]   regmem[0:RegDepth-1];
 
-integer i;
+reg [`RegDataBus-1:0]   regmem[0:RegDepth-1];
+reg [`RegDataBus-1:0] rdata1_r0,rdata2_r0;
+
 always@(*)begin:regmem0  //reg0 is always zero
     regmem[0] = `DataZero;
 end
@@ -27,9 +28,41 @@ always@(posedge clk or negedge rst_n)begin
     end
 end
 
-reg [`RegDataBus-1:0] rdata1_r0,rdata2_r0;
+
 always@(*)begin
-    
+    if (rst_n == `RstEnable) begin
+        rdata1_r0 = `DataZero;
+    end
+    else if (re1) begin
+        if (we && (raddr1 == waddr)) begin
+            rdata1_r0 = wdata;
+        end
+        else begin
+            rdata1_r0 = regmem[raddr1];
+        end
+    end
+    else begin
+        rdata1_r0 = `DataZero;
+    end
 end
+always@(*)begin
+    if (rst_n == `RstEnable) begin
+        rdata1_r0 = `DataZero;
+    end
+    else if (re2) begin
+        if (we && (raddr2 == waddr)) begin
+            rdata2_r0 = wdata;
+        end
+        else begin
+            rdata2_r0 = regmem[raddr2];
+        end
+    end
+    else begin
+        rdata2_r0 = `DataZero;
+    end
+end
+
+assign  rdata1 = rdata1_r0;
+assign  rdata2 = rdata2_r0;
 
 endmodule
